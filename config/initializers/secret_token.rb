d81b556c5372cdb8959fab34a8067b1e566d8e31
Jonathan Hwa <hwaj@hwaj-VirtualBox.(none)>
2013-01-13 22:27:58 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Storifypenn::Application.config.secret_token = '59c58e9bb0e3bbb88dd36c392b235d3742528a7b8f092144ab572741837ce27584d04f68e231a6d80525c4ab7de83dd6965e5a2074a17b23ddf90905ccd8fa15'
