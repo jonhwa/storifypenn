a8248cb079fbf3ad78febb93ee08d0ac435f8149
Jonathan <hwaj@ubuntu.(none)>
2013-02-11 21:42:43 -0500

    Added collection select to locations

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,8 @@
 class Location < ActiveRecord::Base
   belongs_to :user
-  attr_accessible :address, :city, :rate, :state, :zipcode
+  attr_accessible :address, :city, :rate, :state, :zipcode, :user_id
+
+  def user_name
+  	(user.first_name + " " + user.last_name) if user
+  end
 end
