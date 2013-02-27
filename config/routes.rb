f1d0df65d9502be3e799fad1c65fb24f693d0f38
Jonathan Hwa <jonhwa@gmail.com>
2013-02-27 14:05:24 -0500

    Removed :resource users and link to users

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Storifypenn::Application.routes.draw do
 
   resources :locations
 
-  resources :users
+  #resources :users
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
