664c9fbe957e06c893e63fa291ce99585c061a66
Jonathan Hwa <jonhwa@gmail.com>
2013-02-27 03:09:01 -0500

    temporary fix to migration add_devise_to_users for heroku

--- db/migrate/20130226033824_add_devise_to_users.rb
+++ db/migrate/20130226033824_add_devise_to_users.rb
@@ -1,6 +1,6 @@
 class AddDeviseToUsers < ActiveRecord::Migration
   def self.up
-    remove_column :users, :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip
+    remove_column :users, :email
 
     change_table(:users) do |t|
       ## Database authenticatable
